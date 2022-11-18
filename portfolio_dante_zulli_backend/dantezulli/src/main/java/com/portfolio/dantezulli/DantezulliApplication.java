37601c2b8f85516b6eb9d9b7654326ebb76f9d3c
Dante Zulli <dantezulli2004@gmail.com>
2022-11-18 19:28:43 -0300

    Agregué la carpeta del backend
    Hecho con Spring Initializr.
    Dependencias: Web, JPA, MySQL, Validation, DevTools y Lombok

---
+++ portfolio_dante_zulli_backend/dantezulli/src/main/java/com/portfolio/dantezulli/DantezulliApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dantezulli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DantezulliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DantezulliApplication.class, args);
+	}
+
+}
---
+++ portfolio_dante_zulli_backend/dantezulli/src/test/java/com/portfolio/dantezulli/DantezulliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dantezulli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DantezulliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
