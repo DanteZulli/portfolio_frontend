aa1069a52632619e78b8b7e3daea9d5e1af9abbd
Dante Zulli <dantezulli2004@gmail.com>
2022-11-18 19:07:28 -0300

    Dividí mi página estática en componentes
    Dividí mi index.html en 4 componentes principales:
    -NavBar
    -Header
    -Main
    -Footer

--- portfolio_dante_zulli/src/app/app.module.ts
+++ portfolio_dante_zulli/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
