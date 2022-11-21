326df8498a90988566a5e792bdfb28352687ef1a
Dante Zulli <dantezulli2004@gmail.com>
2022-11-21 12:35:12 -0300

    Hecho el Routing para Home y Login

--- portfolio_dante_zulli_frontend/src/app/app.module.ts
+++ portfolio_dante_zulli_frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 // Importamos el modulo de Angular HTTP para conectar el back con el front
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     NavbarComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
