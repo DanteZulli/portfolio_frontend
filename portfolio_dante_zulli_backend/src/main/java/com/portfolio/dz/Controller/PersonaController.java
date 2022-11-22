4ac2253f23ca597f6fbdec2f5f1f2d68a2f3888b
Dante Zulli <dantezulli2004@gmail.com>
2022-11-22 13:09:27 -0300

    Implementados los permisos de Admin
    El back ya funciona completo y acorde a los permisos de usuario.
    Arreglé un problema con un Bean de JWT aplicando un Override.
    Todas las consultas en PostMan van bien.

--- portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Controller/PersonaController.java
+++ portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.dz.Entity.Persona;
 import com.portfolio.dz.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,34 +22,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PersonaController {
-    @Autowired IPersonaService IPersonaService;
-    
+
+    @Autowired
+    IPersonaService IPersonaService;
+
     @GetMapping("/personas/traer")
-    public List<Persona> getPersona(){
+    public List<Persona> getPersona() {
         return IPersonaService.getPersona();
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
-    public String createPersona(@RequestBody Persona persona){
+    public String createPersona(@RequestBody Persona persona) {
         IPersonaService.savePersona(persona);
         return "La persona fue ingresada con éxito.";
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
-    public String deletePersona(@PathVariable Long id){
+    public String deletePersona(@PathVariable Long id) {
         IPersonaService.deletePersona(id);
         return "La persona fue eliminada con éxito.";
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
-    public Persona editPersona(@PathVariable Long id, @RequestParam("nombre") String nuevoNombre, @RequestParam("apellido") String nuevoApellido){
+    public Persona editPersona(@PathVariable Long id, @RequestParam("nombre") String nuevoNombre, @RequestParam("apellido") String nuevoApellido) {
         Persona persona = IPersonaService.findPersona(id);
         persona.setNombre(nuevoNombre);
         persona.setApellido(nuevoApellido);
         IPersonaService.savePersona(persona);
         return persona;
     }
-    
+
     @GetMapping("personas/traer/perfil")
-    public Persona findPersona(){
-        return IPersonaService.findPersona((long)1);
+    public Persona findPersona() {
+        return IPersonaService.findPersona((long) 1);
     }
-    
+
 }
--- portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Security/MainSecurity.java
+++ portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Security/MainSecurity.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -43,13 +44,15 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
-
+    
+    @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
