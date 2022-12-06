1b780bd242a5c71ef15f4c11faf2d0d140cfd286
Dante Zulli <dantezulli2004@gmail.com>
2022-12-06 18:57:54 -0300

    Actualicé el EXP Controller
    Tenía algunos errores

--- portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Controller/ExperienciaController.java
+++ portfolio_dante_zulli_backend/src/main/java/com/portfolio/dz/Controller/ExperienciaController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,8 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dante
  */
 @RestController
-@RequestMapping("exp")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/explab")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExperienciaController {
 
     @Autowired
@@ -48,6 +49,16 @@ public class ExperienciaController {
         Experiencia experiencia = impExperienciaService.getOne(id).get();
         return new ResponseEntity(experiencia, HttpStatus.OK);
     }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!impExperienciaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
+        impExperienciaService.delete(id);
+        return new ResponseEntity(new Mensaje("Experiencia Eliminada"), HttpStatus.OK);
+    }
+    
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoExperiencia dtoexp) {
         if (StringUtils.isBlank(dtoexp.getNombreExp())) {
@@ -78,14 +89,6 @@ public class ExperienciaController {
         experiencia.setDescripcionExp(dtoexp.getDescripcionExp());
         impExperienciaService.save(experiencia);
         return new ResponseEntity(new Mensaje("Experiencia Actualizada"), HttpStatus.OK);
-    }
-
-    public ResponseEntity<?> delete(@PathVariable("id") int id) {
-        if (!impExperienciaService.existsById(id)) {
-            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
-        }
-        impExperienciaService.delete(id);
-        return new ResponseEntity(new Mensaje("Experiencia Eliminada"), HttpStatus.OK);
     }
 
 }
