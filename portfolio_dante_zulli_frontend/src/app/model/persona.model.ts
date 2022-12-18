d940a6a4320185a58874c25d96b8d4870b2c0c8b
Dante Zulli <dantezulli2004@gmail.com>
2022-12-17 21:11:14 -0300

    Persona Model y Service
    Adaptados según los nuevos cambios en el back

--- portfolio_dante_zulli_frontend/src/app/model/persona.model.ts
+++ portfolio_dante_zulli_frontend/src/app/model/persona.model.ts
@@ -3,10 +3,14 @@ export class persona
   id?: number;
   nombre: string;
   apellido: string;
+  descripcion: string;
+  img: string;
 
-  constructor(nombre: string, apellido: string)
+  constructor(nombre: string, apellido: string, descripcion: string, img: string)
   {
     this.nombre = nombre;
     this.apellido = apellido;
+    this.descripcion = descripcion;
+    this.img = img;
   }
 }
--- portfolio_dante_zulli_frontend/src/app/service/persona.service.ts
+++ portfolio_dante_zulli_frontend/src/app/service/persona.service.ts
@@ -8,8 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PersonaService {
   URL = 'http://localhost:8080/personas/';
-  constructor(private http: HttpClient) { }
-  public getPersona(): Observable<persona> {
-    return this.http.get<persona>(this.URL+'traer/perfil');
+  constructor(private httpClient: HttpClient) { }
+  
+  public lista(): Observable<persona[]>{
+    return this.httpClient.get<persona[]>(this.URL + 'lista');
   }
+
+  public detalle(id: number): Observable<persona>{
+    return this.httpClient.get<persona>(this.URL + `detail/${id}`);
+  }
+
+  // public guardar(Persona: persona): Observable<any>{
+  //   return this.httpClient.post<any>(this.URL + 'create', Persona);
+  // }
+
+  public update(id: number, Persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
+  }
+
+  // public delete(id: number): Observable<any>{
+  //   return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+  // }
 }
